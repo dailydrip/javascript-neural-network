ea7a5e63822ac67761f75a061dd8696683286a64
Franzé Jr <franzejr@gmail.com>
2017-10-26 16:55:32 -0300

    Add Recurrent Neural Networks

---
+++ RecurrentNeuralNetworks.js
@@ -0,0 +1,21 @@
+var synaptic = require("synaptic");
+var Neuron = synaptic.Neuron,
+  Trainer = synaptic.Trainer,
+  Architect = synaptic.Architect;
+
+function myNeuralNetwork() {
+  var myNetwork = new Architect.Perceptron(2, 2, 1);
+  var trainer = new Trainer(myNetwork);
+
+  var hopfield = new Architect.Hopfield(10);
+
+  hopfield.learn([
+    [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0]
+  ]);
+
+  console.log(hopfield.feed([0, 1, 0, 1, 0, 1, 0, 1, 1, 1]));
+  console.log(hopfield.feed([1, 1, 1, 1, 1, 0, 0, 1, 0, 0]));
+}
+
+myNeuralNetwork();
